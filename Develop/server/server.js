eaf953304863eb37cb74135a7bc8d75bb97a7403
mayaj0yce <m.j.thayne@gmail.com>
2023-09-13 22:37:40 -0600

    port

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+if (port == null || port = "") {
+    port = 3000;
+}
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
